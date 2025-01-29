8e146b58f424c9f6e71e7a261e708b793909757d
Tiago Fernandes <78868193+tiagofernandes27@users.noreply.github.com>
2025-01-29 00:01:25 +0000

    Initial commit

---
+++ CH5_L1.py
@@ -0,0 +1,52 @@
+#function that was edited
+def total_xp(level, xp_to_add):
+    return (level*100) + xp_to_add
+
+#don't touch below this
+run_cases = [
+    (1, 200, 300),
+    (2, 50, 250),
+]
+
+submit_cases = run_cases + [
+    (0, 0, 0),
+    (0, 200, 200),
+    (176, 350, 17950),
+    (250, 100, 25100),
+]
+
+
+def test(input1, input2, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}, {input2}")
+    print(f"Expecting: {expected_output}")
+    result = total_xp(input1, input2)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()
